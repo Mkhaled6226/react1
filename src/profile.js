48ae96c1100f95f7873a63d0ad2349e3336dba1d
Mkhaled6226 <mkhmkh1130@yahoo.com>
2024-09-25 17:48:15 +0300

    Update profile.js

--- src/profile.js
+++ src/profile.js
@@ -1,10 +1,10 @@
-// import image1 from './images/51.png';
+import image1 from './images/51.png';
 
 const Profile = () => {
   return (
     <div className="profile">
       <h1>Ahmed Sayed Zizo</h1>
-      <img src='./images/51.png' alt="" />
+      <img src={image1} alt="" />
     </div>
   );
 };
